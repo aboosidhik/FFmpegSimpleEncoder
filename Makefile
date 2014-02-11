4fbd7b4c6a0840974a3a672b2391a04760cf5fc3
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2014-02-11 16:17:31 -0200

    First implementation of our encoder
    It is currently too slow to use, but works on Mac OS X and Linux.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CFLAGS=-Wall -Wextra -O2 -D_XOPEN_SOURCE=600
-PLAYER_CFLAGS=$(shell pkg-config --cflags libavformat libavcodec libswscale libavutil sdl)
+PLAYER_CFLAGS=$(shell pkg-config --cflags libavformat libavcodec libswscale libavutil sdl | awk '{gsub(/-I/,"-isystem ");print}')
 PLAYER_LDFLAGS=$(shell pkg-config --libs libavformat libavcodec libswscale libavutil sdl)
 
 .PHONY: all clean
@@ -10,7 +10,7 @@ clean:
 	rm -f clouddisplayplayer clouddisplayencoder
 
 clouddisplayencoder: src/clouddisplayencoder.c
-	$(CC) -std=c99 -o $@ $< $(CFLAGS)
+	$(CC) -std=c99 $(CFLAGS) $(PLAYER_CFLAGS) $< $(PLAYER_LDFLAGS) -o $@
 
 clouddisplayplayer: src/clouddisplayplayer.c
 	$(CC) -std=c99 $(CFLAGS) $(PLAYER_CFLAGS) $< $(PLAYER_LDFLAGS) -o $@
--- src/clouddisplayencoder.c
+++ src/clouddisplayencoder.c
@@ -2,101 +2,259 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libavutil/time.h>
+#include <libswscale/swscale.h>
 
 
-#define kBufferSize 8192
+#define MAX_FDS_OPEN 512
 
-typedef struct {
-    char tag[4];
-    int32_t width;
-    int32_t height;
-} FrameHeader;
+// Only invariants are allowed to be static.
+static int32_t inputWidth = 0;
+static int32_t inputHeight = 0;
+static enum AVPixelFormat inputPixelFormat = AV_PIX_FMT_NONE;
+static size_t inputBytesPerPixel = 0;
 
 
-static size_t pix_fmt_to_bytes_per_pixel(const char* pix_fmt) {
-    // Count the number of '8' in pix_fmt.
-    size_t result = 0;
-    size_t len = strlen(pix_fmt);
-    for (size_t i = 0; i < len; ++i) {
-        if (pix_fmt[i] == '8') ++result;
-    }
-    return result;
+static void sigterm_handler(int sig) {
+  (void)sig; // Supress unused warning.
+  exit(123);
 }
 
 
-static FILE* spawn_ffmpeg(const char* ip, const char* port, const char* pix_fmt,
-                          int width, int height) {
-    if (strcmp(pix_fmt, "RGB888") == 0) {
-        pix_fmt = "rgb24";
-    } else if (strcmp(pix_fmt, "BGR888") == 0) {
-        pix_fmt = "bgr24";
-    } else if (strcmp(pix_fmt, "ABGR8888") == 0) {
-        pix_fmt = "abgr";
-    } else if (strcmp(pix_fmt, "ARGB8888") == 0) {
-        pix_fmt = "argb";
-    } else if (strcmp(pix_fmt, "BGRA8888") == 0) {
-        pix_fmt = "bgra";
-    } else if (strcmp(pix_fmt, "RGBA8888") == 0) {
-        pix_fmt = "rgba";
-    } else {
-        printf("Error! Invalid PIX_FMT: %s\n", pix_fmt);
-        exit(1);
+static size_t pix_fmt_to_bytes_per_pixel(const char* pix_fmt) {
+  // Count the number of '8' in pix_fmt.
+  size_t result = 0;
+  size_t len = strlen(pix_fmt);
+  for (size_t i = 0; i < len; ++i) {
+    if (pix_fmt[i] == '8') ++result;
+  }
+  return result;
+}
+
+
+static enum AVPixelFormat pix_fmt_str_to_enum(const char* pix_fmt) {
+  if (strcmp(pix_fmt, "RGB888") == 0) {
+    return AV_PIX_FMT_RGB24;
+  } else if (strcmp(pix_fmt, "BGR888") == 0) {
+    return AV_PIX_FMT_BGR24;
+  } else if (strcmp(pix_fmt, "ABGR8888") == 0) {
+    return AV_PIX_FMT_ABGR;
+  } else if (strcmp(pix_fmt, "ARGB8888") == 0) {
+    return AV_PIX_FMT_ARGB;
+  } else if (strcmp(pix_fmt, "BGRA8888") == 0) {
+    return AV_PIX_FMT_BGRA;
+  } else if (strcmp(pix_fmt, "RGBA8888") == 0) {
+    return AV_PIX_FMT_RGBA;
+  } else {
+    printf("Error! Invalid PIX_FMT: %s\n", pix_fmt);
+    exit(1);
+  }
+}
+
+
+static void read_frame(AVPicture* picture) {
+  char header[4];
+  memset(&header, 0, sizeof(header));
+
+  if (fread(&header, sizeof(header), 1, stdin) == 1) {
+    if (strncmp(header, "FRM\n", 4) != 0) {
+      fprintf(stderr, "invalid header: %s\n", header);
+      exit(1);
     }
 
-    char command[4096] = {'\0'};
-    snprintf(command, 4096,
-        "ffmpeg -f rawvideo -pix_fmt %s -s %ix%i -r 5 -i - "
-        "-threads 2 -vcodec libx264 -g 5 -preset ultrafast -tune zerolatency "
-        "-crf 30 -f mpegts udp://%s:%s",
-        pix_fmt, width, height, ip, port);
-    printf("command: %s\n", command);
-    return popen(command, "w");
+    size_t pictureSize = inputWidth * inputHeight * inputBytesPerPixel;
+    if (fread(picture->data[0], 1, pictureSize, stdin) != pictureSize) {
+      perror("unable to read frame");
+      exit(1);
+    }
+  } else {
+    perror("unable to read header");
+    exit(1);
+  }
+}
+
+
+static void write_frame(AVCodecContext *encodingContext, AVFormatContext *outputContext, AVPicture* picture) {
+  static struct AVFrame *frame = NULL;
+  static struct SwsContext *sws_ctx = NULL;
+
+  if (!frame) {
+    frame = avcodec_alloc_frame();
+    if (!frame) {
+      fprintf(stderr, "error allocating video frame\n");
+      exit(1);
+    }
+    frame->format = encodingContext->pix_fmt;
+    frame->width  = encodingContext->width;
+    frame->height = encodingContext->height;
+
+    // The image can be allocated by any means and av_image_alloc()
+    // is just the most convenient way if av_malloc() is to be used.
+    if (av_image_alloc(frame->data, frame->linesize, frame->width,
+                       frame->height, frame->format, 32) < 0) {
+      fprintf(stderr, "error allocation video frame data\n");
+      exit(1);
+    }
+  }
+
+  if (!sws_ctx) {
+    sws_ctx = sws_getContext(inputWidth, inputHeight, inputPixelFormat,
+                             frame->width, frame->height, frame->format,
+                             SWS_BICUBIC, NULL, NULL, NULL);
+    if (!sws_ctx) {
+      fprintf(stderr, "Could not initialize the conversion context\n");
+      exit(1);
+    }
+  }
+
+  if (sws_scale(sws_ctx, (const uint8_t * const *)picture->data, picture->linesize,
+                0, inputHeight, frame->data, frame->linesize) <= 0) {
+    fprintf(stderr, "unable to rescale image\n");
+    exit(1);
+  }
+
+  frame->pts = av_gettime();
+
+  AVPacket packet;
+  int got_packet = 0;
+  // encode the image
+  if (avcodec_encode_video2(encodingContext, &packet, frame, &got_packet) < 0) {
+    fprintf(stderr, "error encoding video frame:\n");
+    exit(1);
+  } else {
+    fprintf(stdout, "encoded frame!\n");
+  }
+
+  /* If size is zero, it means the image was buffered. */
+  if (got_packet && packet.size) {
+    packet.stream_index = 0;
+
+    // Write the compressed frame to the media output
+    int err = av_write_frame(outputContext, &packet);
+    if (err < 0) {
+      fprintf(stderr, "unable to write frame: %s\n", av_err2str(err));
+    } else {
+      fprintf(stdout, "wrote frame\n");
+    }
+
+    // Force flushing the output context
+    av_write_frame(outputContext, NULL);
+
+    // Free packet allocated by decoding.
+    av_free_packet(&packet);
+  } else {
+    fprintf(stdout, "buffering data\n");
+  }
 }
 
 
 int main(int argc, char const *argv[]) {
-    if (argc < 4) {
-        printf("%s DEST_IP DEST_PORT PIX_FMT\n", argv[0]);
-        exit(1);
-    }
+  // Check for parameters
+  if (argc < 6) {
+    fprintf(stderr, "%s DEST_IP DEST_PORT WIDTH HEIGHT PIX_FMT\n", argv[0]);
+    return 1;
+  }
 
-    FILE* ffmpeg = NULL;
-    size_t bytes_per_pixel = pix_fmt_to_bytes_per_pixel(argv[3]);
-    uint8_t buffer[kBufferSize] = {0};
+  // Close all file descriptors except the standard ones
+  // This avoids conflitcs between parent context and this one.
+  for (int i = STDERR_FILENO + 1; i < MAX_FDS_OPEN; ++i) {
+    close(i);
+  }
 
-    while (1) {
-        FrameHeader header;
-        memset(&header, 0, sizeof(header));
+  // Register a few signals to avoid blocking forever.
+  signal(SIGINT, sigterm_handler);
+  signal(SIGTERM, sigterm_handler);
 
-        if (fread(&header, sizeof(header), 1, stdin) == 1) {
-            if (strncmp(header.tag, "FRM\n", 4) != 0) {
-                fprintf(stderr, "Invalid header: %s\n", header.tag);
-                exit(1);
-            }
+  // Initialize global parameters
+  char outputAddr[256] = {0};
+  snprintf(outputAddr, sizeof(outputAddr), "udp://%s:%s", argv[1], argv[2]);
+  inputWidth = atoi(argv[3]);
+  inputHeight = atoi(argv[4]);
+  inputPixelFormat = pix_fmt_str_to_enum(argv[5]);
+  inputBytesPerPixel = pix_fmt_to_bytes_per_pixel(argv[5]);
 
-            if (!ffmpeg) {
-                ffmpeg = spawn_ffmpeg(argv[1], argv[2], argv[3], header.width, header.height);
-                if (!ffmpeg) {
-                    perror("unable to spawn ffmpeg");
-                    exit(1);
-                }
-            }
+  // Register all formats and codecs
+  av_register_all();
+  avformat_network_init();
 
-            size_t remaining = (size_t)(header.width * header.height) * bytes_per_pixel;
-            while (remaining > 0) {
-                if (remaining > kBufferSize) {
-                    fread(buffer, 1, kBufferSize, stdin);
-                    fwrite(buffer, 1, kBufferSize, ffmpeg);
-                    remaining -= kBufferSize;
-                } else {
-                    fread(buffer, 1, remaining, stdin);
-                    fwrite(buffer, 1, remaining, ffmpeg);
-                    remaining = 0;
-                }
-            }
-        } else {
-            perror("unable to read header");
-            exit(1);
-        }
-    }
+  // Alloc context for outputting the data.
+  AVFormatContext *outputContext = NULL;
+  avformat_alloc_output_context2(&outputContext, NULL, "mpegts", outputAddr);
+  if (!outputContext) {
+    fprintf(stderr, "error allocating output context\n");
+    return 1;
+  }
+
+  // Find the H.264 encoder. The `encoder` struct must be "opened" before using.
+  AVCodec *encoder = avcodec_find_encoder_by_name("libx264");
+  if (!encoder) {
+    fprintf(stderr, "x264 encoder not found\n");
+    return 1;
+  }
+
+  // Add an stream to the output. This stream will contain video frames.
+  AVStream *stream = avformat_new_stream(outputContext, encoder);
+  if (!stream) {
+    fprintf(stderr, "error when creating stream\n");
+    return 1;
+  }
+  // Configure the stream ID (needed for transmitting it)
+  stream->id = outputContext->nb_streams - 1;
+
+  // Grab the encoding context from format.
+  AVCodecContext *encodingContext = stream->codec;
+
+  // Resolution must be a multiple of two
+  encodingContext->width = inputWidth;
+  encodingContext->height = inputHeight;
+  // Set default encoding parameters
+  encodingContext->bit_rate = 400000;
+  encodingContext->time_base.num = 1;
+  encodingContext->time_base.den = 10;
+  encodingContext->gop_size = 1; // emit only intra frames
+  encodingContext->pix_fmt = AV_PIX_FMT_YUV420P;
+
+  // Set the same presets as in the command line
+  av_opt_set(encodingContext->priv_data, "preset", "ultrafast", 0);
+  av_opt_set(encodingContext->priv_data, "tune", "zerolatency", 0);
+
+  // Open encoding context for our encoder
+  if (avcodec_open2(encodingContext, encoder, NULL) < 0) {
+    fprintf(stderr, "error opening encoder\n");
+    return 1;
+  }
+
+  // Open output buffer. This will also open the UDP socket.
+  if (avio_open(&outputContext->pb, outputAddr, AVIO_FLAG_WRITE) < 0) {
+    fprintf(stderr, "error opening output buffer\n");
+    return 1;
+  }
+
+  // Write transport stream header (PAT, PMT, etc).
+  // This segfaults without avio_open.
+  if (avformat_write_header(outputContext, NULL) < 0) {
+    fprintf(stderr, "error writing mpegts header\n");
+    return 1;
+  }
+
+  // Allocate picture so it can be correcly aligned.
+  AVPicture *inputPicture = calloc(1, sizeof(AVPicture));
+  if (av_image_alloc(inputPicture->data, inputPicture->linesize,
+                     inputWidth, inputHeight, inputPixelFormat, 32) < 0) {
+    fprintf(stderr, "error allocating input picture\n");
+    return 1;
+  }
+
+  while (1) {
+    fprintf(stderr, "Reading frame\n");
+    read_frame(inputPicture);
+    fprintf(stderr, "Encoding frame\n");
+    write_frame(encodingContext, outputContext, inputPicture);
+  }
 }
