a6fcd265d81de8675b5efdf51840cdbf993a16b4
Eduardo Felipe Castegnaro <edufelipe@e3c.com.br>
2014-09-25 17:41:39 +0200

    Make test script run_player.py configurable

--- run_player.py
+++ run_player.py
@@ -1,17 +1,25 @@
+import argparse
 import struct
 import subprocess
 import time
 
 
 if __name__ == '__main__':
-    ffmpeg = subprocess.Popen(['./clouddisplayplayer', '10.0.33.37', '8000'], stdin=subprocess.PIPE)
+    parser = argparse.ArgumentParser(description='Sample spawner for clouddisplayplayer', conflict_handler='resolve')
+    parser.add_argument('--player', default='./clouddisplayplayer', metavar='PATH')
+    parser.add_argument('-x', default=100, type=int, metavar='POS_X')
+    parser.add_argument('-y', default=100, type=int, metavar='POS_Y')
+    parser.add_argument('-w', default=320, type=int, metavar='WIDTH')
+    parser.add_argument('-h', default=180, type=int, metavar='HEIGHT')
+    parser.add_argument('-p', '--port', default='8000')
+    parser.add_argument('host')
 
-    ffmpeg.stdin.write(struct.pack('4siiii', 'POS\n', 100, 100, 100, 100))
-    ffmpeg.stdin.flush()
+    args = parser.parse_args()
 
-    time.sleep(20)
+    player = subprocess.Popen([args.player, args.host, args.port], stdin=subprocess.PIPE)
 
-    ffmpeg.stdin.write(struct.pack('4siiii', 'POS\n', 200, 200, 200, 200))
-    ffmpeg.stdin.flush()
+    player.stdin.write(struct.pack('4siiii', 'POS\n', args.x, args.y, args.w, args.h))
+    player.stdin.flush()
 
-    time.sleep(60)
+    while True:
+        time.sleep(600)
